5568a0644408d96dd6d625a2a2387332daf7cdaa
thekopiman <34156149+thekopiman@users.noreply.github.com>
2023-07-26 15:29:14 +0800

    Update extract.py
    Fixed the bug

--- yolov5/data_utils/extract.py
+++ yolov5/data_utils/extract.py
@@ -21,7 +21,7 @@ class Extraction:
         self.paths = None
         self.obtain_classes(path=path)  # Add the new function next time
 
-    def obtain_files(self, path: str, file_type: str = "csv") -> list[str]:
+    def obtain_files(self, path: str, file_type: str = "csv") -> any:
         """
         Obtain all the specific files from the folder ending with file_type
 
@@ -86,7 +86,7 @@ class Extraction:
         bt_dict.update({"BLE_1MHz_AIND": counter})
         return bt_dict
 
-    def obtain_classes(self, path: str = None) -> dict[str, int]:
+    def obtain_classes(self, path: str = None) -> any:
         """
         Reads from yaml file if possible
         or else will create the classes on the fly
@@ -264,157 +264,126 @@ def version_update(file_name: str, dir: str) -> str:
     return f"{prefix}_v{ver_int}.{suffix}"
 
 
-def get_parser() -> any:
-    """Parser
-
-    Returns:
-        parser: Haven't .parse_arg()
-    """
-    parser = argparse.ArgumentParser(
-        "Extraction", description="Used in extracting various signals"
-    )
-
-    # Mode
-    parser.add_argument(
-        "-m",
-        "--mode",
-        default="false",
-        type=str,
-        help="Extraction modes: classes18, getsignals, false",
-    )
-
-    # Classes 18 - Splitting the 3 classes into 18 different classes while removing collision
-    parser.add_argument("-y", "--yaml", type=str, help="yaml path")
-    parser.add_argument(
-        "-data", "--data", type=str, help="path of spectrogram, the 80gb folder"
-    )
-    parser.add_argument(
-        "-dest", "--dest", type=str, help="destination path of the labels files"
-    )
-    parser.add_argument(
-        "-dupli",
-        "--duplicate-imgs",
-        type=str,
-        default=None,
-        help="Duplicate the *.png files (not marked) files into a path",
-    )
-    parser.add_argument(
-        "-addnew",
-        "--addnew",
-        default=False,
-        type=lambda x: (str(x).lower() == "true"),
-        help="add *_new to the end of each label file",
-    )
-    parser.add_argument(
-        "-addresultframe",
-        "--addresultframe",
-        default=True,
-        type=lambda x: (str(x).lower() == "true"),
-        help="boolean for adding the word 'result_frame' to the start of the label name",
-    )
-
-    parser.add_argument(
-        "-cap",
-        "--cap",
-        type=int,
-        default=-1,
-        help="Caps/Limits the number of images being extract during 'getsignals'. Defaults to -1, basically everything.",
-    )
-
-    # Generate train/val/test.txt files
-    parser.add_argument(
-        "-generate",
-        "--generate",
-        type=str,
-        default=None,
-        help="Generate the train/val/test files from the images folder. Fixed at 70:20:10. Ensure that the folder do not include _marked.png pics as I'm searching for *.png. Note: If you set this arg as None but duplicate has a path, then it will automatically use the duplicate path.",
-    )
-    # Get signals - Extract out every signal as a png file
-
-    parser.add_argument(
-        "-imgs",
-        "--imgs-path",
-        type=str,
-        help="Src Path of the images. Ensure that the name of the label and imgs are identical (excluding the .png/.txt).",
-    )
-    parser.add_argument(
-        "-labels",
-        "--labels-path",
-        type=str,
-        help="Src Path of the labels. Ensure that the name of the label and imgs are identical (excluding the .png/.txt).",
-    )
-
-    return parser
-
-
-def main(args: dict) -> None:
-    """
-    main function
-
-    Args:
-        args (dict): Contains the args from the argparse
-
-    Raises:
-        Exception: Missing paths
-    """
-    if args["duplicate_imgs"] != None:
-        assert args["duplicate_imgs"] != None, "Missing duplicate imgs path"
-        assert args["data"] != None, "Missing data path"
-
-        import shutil
-
-        # print(args['duplicate_imgs'], os.path.exists(args['duplicate_imgs']))
-        if not os.path.exists(args["duplicate_imgs"]):
-            print("----- Directory Not Found, creating new directory ------")
-            os.makedirs(args["duplicate_imgs"])
-
-        print("------- Currently Duplicating images ---------")
-        folder_size = len(glob.glob(os.path.join(args["data"], "results", "*+6.png")))
-        division_size = folder_size / 4
-        for idx, file in enumerate(
-            glob.glob(os.path.join(args["data"], "results", "*+6.png"))
-        ):  # Debugging
-            if idx % division_size == 0:
-                print(f"---- Progress {idx*25}% ----")
-            shutil.copy(file, args["duplicate_imgs"])
-
-    if args["generate"] != None or args["duplicate_imgs"] != None:
-        gen_path = (
+class Parser():
+    def __init__(self):
+        """
+        Parser class for Extract since it's getting too big
+        
+        Args: None
+        
+        Raises:
+            Exception: Missing paths
+        """
+        self.args = None
+        self.get_parser()
+    
+    # @classmethod
+    def main(self, args : dict = None):
+        if not isinstance(args, dict):
+            args = self.args
+        
+        # DUPLICATE IMGS
+        if args["duplicate_imgs"] != None:
+            assert args["duplicate_imgs"] != None, "Missing duplicate imgs path"
+            assert args["data"] != None, "Missing data path"
+            self.duplicate_imgs(args)
+        
+        # GENERATE .txt FILES
+        if args["generate"] != None or args["duplicate_imgs"] != None:
+            gen_path = (
             args["generate"] if args["generate"] != None else args["duplicate_imgs"]
         )
+        
+            save_path = args['data'] if args['data'] != None else os.getcwd()
+            self.generate(gen_path = gen_path, save_path = save_path)
+        
+        # Running of the various modes
+        
+        # classes 18
+        
+        if args["mode"] == "classes18":
+            assert args["yaml"] != None, "Missing yaml path"
+            assert args["data"] != None, "Missing data path"
+            
+            try:
+                assert args["dest"] != None, "Missing dest path"
+            
+            except AssertionError:
+                counter = 0
+                while os.path.exists(os.path.join(args["data"], f"dataset{counter}")):
+                    counter += 1
 
-        files = glob.glob(os.path.join(gen_path, "*.png"))
-        length = len(files)
+                args["dest"] =  os.path.join(args["data"], f"dataset{counter}")
+                os.makedirs(args["dest"])
 
-        train_int = int(0.7 * length)
-        val_int = int(0.2 * length) + train_int
-        # test_int = int(0.1*length) + val_int
+                args["dest"] = os.path.join(args["dest"], "labels")
+                os.makedirs(args["dest"])
 
-        with open("train_list.txt", "w+") as w:
-            for l in files[:train_int]:
-                w.writelines(l)
-                w.writelines("\n")
+                print(f"--- Dest Directory Not Found, creating new directory: {args['dest']} ----")
+            
+            self.args = args
+            self.classes18(args)
+            
+        # getsignals
+        
+        elif args["mode"] == "getsignals":
+            assert args["yaml"] != None, "Missing yaml path"
+            assert args["dest"] != None, "Missing dest path"
+            assert args["labels_path"] != None, "Missing labels path"
+            assert args["imgs_path"] != None, "Missing imgs path"
+            self.getsignals(args)
+        
+        
+        
+        # Automatically run classes18 and getsignals and also duplicate files 
+        # Only yaml and data path are required here
+        elif args["mode"] == "auto":
+            assert args["yaml"] != None, "Missing yaml path"
+            assert args["data"] != None, "Missing data path"
+            
+            counter = 0
+            while os.path.exists(os.path.join(args["data"], f"dataset{counter}")):
+                counter += 1
 
-        with open("val_list.txt", "w+") as w:
-            for l in files[train_int:val_int]:
-                w.writelines(l)
-                w.writelines("\n")
+            args["dest"] =  os.path.join(args["data"], f"dataset{counter}")
+            os.makedirs(args["dest"])
+            
+            print(f"--- classes18 directory is {args['dest']} ---")
+            
+            args["duplicate_imgs"] = os.path.join(args["dest"], "images")
+            
+            self.duplicate_imgs(args)
+            
+            # Got bug here
+            self.generate(gen_path = args["duplicate_imgs"], save_path = args["data"])
+            
+            args["dest"] = os.path.join(args["dest"], "labels")
+            os.makedirs(args["dest"])
+            
+            # Start classes18
+            self.classes18(args)
+            
+            
+            # Start getsignals
+            args["labels_path"] = args["dest"]
+            args["imgs_path"] = args["duplicate_imgs"]
+            
+            while os.path.exists(os.path.join(args["data"], f"dataset{counter}")):
+                counter += 1
 
-        with open("test_list.txt", "w+") as w:
-            for l in files[val_int:]:
-                w.writelines(l)
-                w.writelines("\n")
-
-        print(
-            f"train/val/test_list.txt files have been saved at {os.getcwd()}."
-        )  # Implement new feature here
-
-    # Running of the mode
-    if args["mode"] == "classes18":
-        assert args["yaml"] != None, "Missing yaml path"
-        assert args["data"] != None, "Missing data path"
-        assert args["dest"] != None, "Missing dest path"
-
+            args["dest"] =  os.path.join(args["data"], f"dataset{counter}")
+            os.makedirs(args["dest"])
+            print(f"--- getsignals directory is {args['dest']} ---")
+            
+            self.args = args
+            self.getsignals(args)
+            
+        elif args["mode"] == "false": 
+            pass
+        else:
+            raise Exception("Mode name is wrong") 
+        
+    def classes18(self, args : dict) -> None:
         print("------- Signal Classification has began -------")
 
         labels_more_classification = Extraction(path=args["yaml"])
@@ -441,13 +410,8 @@ def main(args: dict) -> None:
                 labels_more_classification.extract(
                     filepath, dest_path, txt_file_path, add_new, add_result_frame
                 )
-
-    elif args["mode"] == "getsignals":
-        assert args["yaml"] != None, "Missing yaml path"
-        assert args["dest"] != None, "Missing dest path"
-        assert args["labels_path"] != None, "Missing labels path"
-        assert args["imgs_path"] != None, "Missing imgs path"
-
+        
+    def getsignals(self, args : dict) -> None:
         print("------- Signals will now be extracted -------")
 
         import cv2
@@ -487,8 +451,12 @@ def main(args: dict) -> None:
 
             index = image.replace(args["imgs_path"], "")
             index = index.replace(".png", "")
-
-            label_path = os.path.join(args["labels_path"], f"{index}.txt")
+            
+            # print("labels_path", args["labels_path"])
+            
+            
+            # Cannot use os.path.join for some reason
+            label_path = f"{args['labels_path']}{os.path.sep}{index}.txt"
             # print(label_path)
 
             # print("OS path Joined", os.path.join(args['labels_path'], f"{index}.txt"))
@@ -523,15 +491,141 @@ def main(args: dict) -> None:
                         )
                     except:
                         print(classes[label], final_img.shape)
+    
+    def duplicate_imgs(self, args : dict = None):
+        import shutil
 
-    elif args["mode"] == "false":
-        pass
+        # print(args['duplicate_imgs'], os.path.exists(args['duplicate_imgs']))
+        if not os.path.exists(args["duplicate_imgs"]):
+            print("----- Directory Not Found, creating new directory ------")
+            os.makedirs(args["duplicate_imgs"])
 
-    else:
-        raise Exception("Mode name is wrong")
+        print("------- Currently Duplicating images ---------")
+        folder_size = len(glob.glob(os.path.join(args["data"], "results", "*+6.png")))
+        division_size = folder_size / 4
+        for idx, file in enumerate(
+            glob.glob(os.path.join(args["data"], "results", "*+6.png"))
+        ):  # Debugging
+            if idx % division_size == 0:
+                print(f"---- Progress {idx/division_size *25}% ----")
+            shutil.copy(file, args["duplicate_imgs"])
+    
+    def generate(self, gen_path : str, save_path : str):
+        files = glob.glob(os.path.join(gen_path, "*.png"))
+        length = len(files)
+
+        train_int = int(0.7 * length)
+        val_int = int(0.2 * length) + train_int
+        # test_int = int(0.1*length) + val_int
+
+        with open(os.path.join(save_path,"train_list.txt"), "w+") as w:
+            for l in files[:train_int]:
+                w.writelines(l)
+                w.writelines("\n")
+
+        with open(os.path.join(save_path,"val_list.txt"), "w+") as w:
+            for l in files[train_int:val_int]:
+                w.writelines(l)
+                w.writelines("\n")
+
+        with open(os.path.join(save_path,"test_list.txt"), "w+") as w:
+            for l in files[val_int:]:
+                w.writelines(l)
+                w.writelines("\n")
+
+        print(
+            f"train/val/test_list.txt files have been saved at {save_path}."
+        )  # Implement new feature here
+    
+    def __repr__(self) -> dict:
+        return self.args
+    
+    
+    def get_parser(self) -> any:
+        """Parser
+
+        Returns:
+            parser: Haven't .parse_arg()
+        """
+        parser = argparse.ArgumentParser(
+            "Extraction", description="Used in extracting various signals"
+        )
+
+        # Mode
+        parser.add_argument(
+            "-m",
+            "--mode",
+            default="false",
+            type=str,
+            help="Extraction modes: classes18, getsignals, false, auto",
+        )
+
+        # Classes 18 - Splitting the 3 classes into 18 different classes while removing collision
+        parser.add_argument("-y", "--yaml", type=str, help="yaml path")
+        parser.add_argument(
+            "-data", "--data", type=str, help="path of spectrogram, the 80gb folder"
+        )
+        parser.add_argument(
+            "-dest", "--dest", type=str, help="destination path of the labels files"
+        )
+        parser.add_argument(
+            "-dupli",
+            "--duplicate-imgs",
+            type=str,
+            default=None,
+            help="Duplicate the *.png files (not marked) files into a path",
+        )
+        parser.add_argument(
+            "-addnew",
+            "--addnew",
+            default=False,
+            type=lambda x: (str(x).lower() == "true"),
+            help="add *_new to the end of each label file",
+        )
+        parser.add_argument(
+            "-addresultframe",
+            "--addresultframe",
+            default=True,
+            type=lambda x: (str(x).lower() == "true"),
+            help="boolean for adding the word 'result_frame' to the start of the label name",
+        )
+
+        parser.add_argument(
+            "-cap",
+            "--cap",
+            type=int,
+            default=-1,
+            help="Caps/Limits the number of images being extract during 'getsignals'. Defaults to -1, basically everything.",
+        )
+
+        # Generate train/val/test.txt files
+        parser.add_argument(
+            "-generate",
+            "--generate",
+            type=str,
+            default=None,
+            help="Generate the train/val/test files from the images folder. Fixed at 70:20:10. Ensure that the folder do not include _marked.png pics as I'm searching for *.png. Note: If you set this arg as None but duplicate has a path, then it will automatically use the duplicate path.",
+        )
+        # Get signals - Extract out every signal as a png file
+
+        parser.add_argument(
+            "-imgs",
+            "--imgs-path",
+            type=str,
+            help="Src Path of the images. Ensure that the name of the label and imgs are identical (excluding the .png/.txt).",
+        )
+        parser.add_argument(
+            "-labels",
+            "--labels-path",
+            type=str,
+            help="Src Path of the labels. Ensure that the name of the label and imgs are identical (excluding the .png/.txt).",
+        )
+        
+        self.args = vars(parser.parse_args())
+        return parser
+        
 
 
 if __name__ == "__main__":
-    args = get_parser().parse_args()
-    args = vars(args)
-    main(args)
+    args = Parser()
+    args.main()
